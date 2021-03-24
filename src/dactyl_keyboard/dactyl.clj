92cdddaa1b537c478e24a7980112f20c8a5b5c25
Sky Rose <zgnomdnv@sky.skyqrose.com>
2021-03-24 05:11:44 -0400

    remove unused dependency

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -2,8 +2,7 @@
   (:refer-clojure :exclude [use import])
   (:require [clojure.core.matrix :refer [array matrix mmul]]
             [scad-clj.scad :refer :all]
-            [scad-clj.model :refer :all]
-            [unicode-math.core :refer :all]))
+            [scad-clj.model :refer :all]))
 
 
 (defn deg2rad [degrees]
